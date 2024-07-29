e5bf5dbfc1f1a5545e37202c2f0f18255cb2a90f
szogun1910 <69729783+szogun1910@users.noreply.github.com>
2024-07-29 20:00:56 +0200

    Create stats.php
    polish translation

---
+++ i18n/pl/stats.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+        'title' => 'Statystyki gracza',
+        'description' => 'Na tej stronie możesz zobaczyć globalne statystyki graczy',
+        'module_description' => 'Ten moduł wyświetla statystyki gracza z serwera',
+        'server_not_found' => 'Nie znaleziono serwera',
+        'params_empty' => 'Niektóre parametry są puste',
+        'admin' => [
+            'title' => 'Moduł statystyk',
+        ],
+        'profile' => [
+            'no_info' => 'Brak dostępnych informacji 😢',
+            'user' => 'Statystyki gracza - :name',
+            'head' => 'Statystyki',
+            'value' => 'Punkty',
+            'kills' => 'Zabójstwa',
+            'deaths' => 'Śmierci',
+            'strzelby' => 'Strzały',
+            'experience' => 'Doświadczenie',
+            'score' => 'Wynik',
+            'hits' => 'Trafienia',
+            'headshots' => 'Strzały w głowę',
+            'assists' => 'Asystwy',
+            'round_win' => 'Wygrane rundy',
+            'round_lose' => 'Przegrane rundy',
+        ],
+        'name' => 'Nazwa',
+        'rank' => 'Ranga',
+        'score' => 'Wynik',
+        'kills' => 'Zabójstwa',
+        'deaths' => 'Śmierci',
+        'last_active' => 'Ostatnia aktywność',
+        'experience' => 'Doświadczenie',
+        'choose_server' => 'Wybierz serwer'
+];
